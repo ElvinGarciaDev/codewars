bf8144eaea3cdf9aa7a2a53f7c50975139630848
Elvin Garcia <elvingarcia19@gmail.com>
2022-09-20 14:50:53 -0400

    codewars

---
+++ 8kyu/Java/ConvertANumberToAString/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(numberToString(5));
+
+    }
+
+    // Functions turns number into a String
+    public static String numberToString(int num) {
+        return num + "";
+    }
+}
