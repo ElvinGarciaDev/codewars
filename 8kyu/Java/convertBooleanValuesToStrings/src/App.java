46f7056949d3475b4a0baefb83c7b3f73abe8ed9
Elvin Garcia <elvingarcia19@gmail.com>
2022-09-21 22:18:08 -0400

    updated c++

--- 8kyu/Java/convertBooleanValuesToStrings/src/App.java
+++ 8kyu/Java/convertBooleanValuesToStrings/src/App.java
@@ -8,3 +8,9 @@ public class App {
       return b == true ? "Yes" : "No";
     }
 }
+
+
+/*
+ * Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+ * 
+ */
