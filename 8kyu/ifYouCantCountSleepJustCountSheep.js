11c342a72c7474479b25f807f226c19e9e0ef931
Elvin Garcia <elvingarcia19@gmail.com>
2022-10-10 16:07:22 -0400

    finished ifYouCantCountSleepJustCountSheep.js

---
+++ 8kyu/ifYouCantCountSleepJustCountSheep.js
@@ -0,0 +1,15 @@
+/*
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+*/
+
+var countSheep = function (num){
+    //your code here
+    let str = '';
+    
+    for (let i = 0; i < num; i++) {
+      str += `${i + 1} sheep...`
+    }
+    
+    return str
+}
+  
