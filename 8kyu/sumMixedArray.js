d74c84e645901ddcb01feed1c492c1bbfd45662b
Elvin Garcia <elvingarcia19@gmail.com>
2022-10-20 17:20:26 -0400

    finished sumMixedArray.js

---
+++ 8kyu/sumMixedArray.js
@@ -0,0 +1,9 @@
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+
+// The reduce method can be used to find the sum of an array, The Number method can make sure any strings get converted into numbers
+let sumMix = x => x.reduce((previous, current) => previous + Number(current), 0)
