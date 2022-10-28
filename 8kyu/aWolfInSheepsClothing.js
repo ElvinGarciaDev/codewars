12f14c00f8f8bfe4529696378dd4a05651c3ef55
Elvin Garcia <elvingarcia19@gmail.com>
2022-10-27 20:40:44 -0400

    aWolfInSheepsClothing.js

---
+++ 8kyu/aWolfInSheepsClothing.js
@@ -0,0 +1,8 @@
+function warnTheSheep(queue) {
+
+    // Reverse the array
+    let arr = queue.reverse()
+
+    return arr[0] === 'wolf' ? "Pls go away and stop eating my sheep" : `Oi! Sheep number ${wolfindex = arr.indexOf('wolf')}! You are about to be eaten by a wolf!`
+
+}
