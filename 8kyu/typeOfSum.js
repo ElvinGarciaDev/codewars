b58470c82ba1e5f5675e380852442a219aabbbdc
Elvin Garcia <elvingarcia19@gmail.com>
2022-12-19 21:50:44 -0500

    finished typeOfSum.js

---
+++ 8kyu/typeOfSum.js
@@ -0,0 +1,13 @@
+/*
+Return the type of the sum of the two arguments
+
+
+*/
+
+function typeOfSum(a, b) {
+    if(typeof a == 'string' || typeof b == 'string') {
+      return 'string'
+    }else {
+      return 'number'
+    }
+}
