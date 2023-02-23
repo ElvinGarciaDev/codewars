bce88a4bdd5ecd7bbdbdd0fb70858d29975e8cab
Elvin Garcia <elvingarcia19@gmail.com>
2023-02-22 20:52:21 -0500

    finished detectPangram.js in a different way

--- 6kyu/detectPangram.js
+++ 6kyu/detectPangram.js
@@ -29,3 +29,21 @@ function isPangram(string){
     // Return true if the loop above runs without returning false
     return true
 }
+
+// Or
+
+function pangramChecker(string) {
+
+    str = string.toLowerCase()
+
+    let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    for(let i = 0; i < letters.length; i++){
+        if (str.indexOf(letters[i]) == -1) {
+            return false;
+          }
+    }
+
+    return true
+
+}
