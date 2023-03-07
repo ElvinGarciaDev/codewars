37614c5fbe032d16cf51037b2624f14cab71f063
Elvin Garcia <elvingarcia19@gmail.com>
2023-03-06 22:39:11 -0500

    finished theWideMouthedFrog.js

---
+++ 8kyu/theWideMouthedFrog.js
@@ -0,0 +1,9 @@
+function mouthSize(animal) {
+  // conditional. Check if animal passed in is an alligator
+  if (animal.toLowerCase() == "alligator") {
+    // return "wide"
+    return "small";
+  } else {
+    return "wide";
+  }
+}
