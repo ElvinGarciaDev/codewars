cc00ccc8a680d85494dae17cc76c67888fdf6432
Elvin Garcia <elvingarcia19@gmail.com>
2023-03-18 22:02:27 -0400

    finished fizzBuzz.js

---
+++ 7kyu/fizzBuzz.js
@@ -0,0 +1,43 @@
+/*
+Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value "Fizz" instead
+If the value is a multiple of 5: use the value "Buzz" instead
+If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+N will never be less than 1.
+
+Method calling example:
+
+fizzbuzz(3) -->  [1, 2, "Fizz"]
+*/
+
+// Return an array
+function fizzbuzz(n)
+{
+  let arr = []
+
+  for(let i = 1; i <= n; i++) {
+    let str = ''
+
+    if(i % 3 == 0) {
+        str += "Fizz"
+    }
+
+    if(i % 5 == 0) {
+        str += "Buzz"
+    }
+
+    if(str == '') {
+        str = i
+    }
+
+    arr.push(str)
+  }
+
+  return arr
+}
+
+
+console.log(fizzbuzz(10))
