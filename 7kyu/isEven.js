c6c891f120a12eabc1ea2065e73ef34fc2dc5709
Elvin Garcia <elvingarcia19@gmail.com>
2023-11-04 16:47:03 -0400

    finished isEven.js

---
+++ 7kyu/isEven.js
@@ -0,0 +1,20 @@
+/*
+Is the number even?
+If the numbers is even return true. If it's odd, return false.
+
+
+
+
+
+Oh yeah... the following symbols/commands have been disabled!
+
+use of %
+use of .even? in Ruby
+use of mod in Python
+*/
+
+
+let isEven = n => {
+    let num = n / 2
+    return  Number.isInteger(num)
+}
