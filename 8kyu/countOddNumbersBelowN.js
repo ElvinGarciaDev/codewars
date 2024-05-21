59a3c2f06816f746f8d87531ff4b220439a15f4b
Elvin Garcia <elvingarcia19@gmail.com>
2024-05-21 13:37:36 -0400

    finished codewar

---
+++ 8kyu/countOddNumbersBelowN.js
@@ -0,0 +1,23 @@
+/*
+
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+Examples (Input -> Output)
+7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+Expect large Inputs!
+
+
+ */
+
+const oddCount = n => {
+    let oddNums = 0
+
+    for(let i = 0; i < n; i++) {
+        if(i % 2 != 0) {
+            oddNums += 1
+        }
+    }
+
+    return oddNums
+}
