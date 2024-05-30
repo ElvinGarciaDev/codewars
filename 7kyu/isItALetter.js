d6abf1492adc340fd7c3d2474d84011d2e5c5048
Elvin Garcia <elvingarcia19@gmail.com>
2024-05-29 20:32:16 -0400

    isItALetter.js

--- 7kyu/isItALetter.js
+++ 7kyu/isItALetter.js
@@ -5,5 +5,8 @@ Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a 
 */
 
 function isItLetter(character) {
-    return true || false ;
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    return alphabet.includes(character.toLowerCase()) ? true : false
+
 }
