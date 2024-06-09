e2d53b2a4c4a732908f457c0b59d38a0e3a01890
Elvin Garcia <elvingarcia19@gmail.com>
2024-06-09 14:21:42 -0400

    sortedYesNo.js

--- 7kyu/sortedYesNo.js
+++ 7kyu/sortedYesNo.js
@@ -6,3 +6,20 @@ Complete the method which accepts an array of integers, and returns one of the f
 "no" - otherwise
 You can assume the array will always be valid, and there will always be one correct answer.
 */
+
+const isSortedAndHow = (array) => {
+
+    let ascending = [...array].sort((a, b) => a - b)
+    let descending = [...array].sort((a, b) => b - a)
+
+    if(array.every((value, index) => value === ascending[index])) {
+        return "yes, ascending"
+    }else if (array.every((value, index) => value === descending[index])) {
+        return "yes, descending"
+    }else {
+        return "no"
+    }
+
+
+
+}
