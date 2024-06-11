0cf9c1254a2d096f4b156cb408cd6b2fe8c51f86
Elvin Garcia <elvingarcia19@gmail.com>
2024-06-11 13:16:42 -0400

    codewar isReallyNaN.js

--- 8kyu/countOddNumbersBelowN.js
+++ 8kyu/countOddNumbersBelowN.js
@@ -10,14 +10,8 @@ Expect large Inputs!
 
  */
 
-const oddCount = n => {
-    let oddNums = 0
 
-    for(let i = 0; i < n; i++) {
-        if(i % 2 != 0) {
-            oddNums += 1
-        }
-    }
 
-    return oddNums
+function oddCount(n) {
+    return Math.floor(n / 2);
 }
