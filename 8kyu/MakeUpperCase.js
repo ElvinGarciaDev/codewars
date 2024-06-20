769b64c6a74797a2899e560c773cd4ce1e0d0fea
Elvin Garcia <elvingarcia19@gmail.com>
2024-06-19 22:43:33 -0400

    typescript

--- 8kyu/MakeUpperCase.js
+++ 8kyu/MakeUpperCase.js
@@ -1,7 +1,13 @@
+"use strict";
 /*
 Write a function which converts the input string to uppercase.
-*/
 
+
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.makeUpperCase = void 0;
 function makeUpperCase(str) {
-    return str.toUpperCase()
+    return str.toUpperCase();
 }
+exports.makeUpperCase = makeUpperCase;
+console.log(makeUpperCase("elvin"));
---
+++ 8kyu/makeUpperCase.ts
@@ -0,0 +1,9 @@
+/*
+Write a function which converts the input string to uppercase.
+
+
+*/
+
+export function makeUpperCase(str: string): string{
+    return str.toUpperCase();
+  }
